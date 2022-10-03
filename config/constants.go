38d5a36d1dc19bf2be64d5b19b88c8f5aa64020c
“Nawi” <nawihusen@gmail.com>
2022-10-04 01:21:12 +0800

    config

--- config/constants.go
+++ config/constants.go
@@ -4,3 +4,10 @@ const SECRET_JWT = "HAS3S"
 const DEFAULT_PROFILE = "https://capstone-happyapp.s3.ap-southeast-1.amazonaws.com/blank-profile-picture-gd0d4ba4a8_1280.png"
 const DEFAULT_COMMUNITY = "https://capstone-happyapp.s3.ap-southeast-1.amazonaws.com/pngwing.com.png"
 const DEFAULT_PRODUCT = "https://capstone-happyapp.s3.ap-southeast-1.amazonaws.com/no-photo-available.png"
+
+const GOPAY = "GOPAY"
+const BCA_VIRTUAL_ACCOUNT = "BCA_VIRTUAL_ACCOUNT"
+const MANDIRI_VIRTUAL_ACCOUNT = "MANDIRI_VIRTUAL_ACCOUNT"
+
+const EVENT = "EVENT"
+const TRANSACTION = "TRANSACTION"
