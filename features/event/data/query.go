78be9778927cfb93aa61ea416977afd3835e4f48
Amin Rais <muhammadamin.rais1@gmail.com>
2022-10-03 09:42:55 +0700

    resolved event

--- features/event/data/query.go
+++ features/event/data/query.go
@@ -62,14 +62,14 @@ func (repo *eventData) SelectEvent(search string) ([]event.Response, error) {
 
 	var dataEvent []event.Response
 	if search == "" {
-		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("inner join join_events on join_events.event_id = events.id").Group("events.id").Scan(&dataEvent)
+		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("right join join_events on join_events.event_id = events.id").Group("events.id").Scan(&dataEvent)
 		if tx.Error != nil {
 			return nil, tx.Error
 		}
 
 		return dataEvent, nil
 	} else {
-		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("inner join join_events on join_events.event_id = events.id").Where("events.title like ?", ("%" + search + "%")).Group("events.id").Scan(&dataEvent)
+		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("right join join_events on join_events.event_id = events.id").Where("events.title like ?", ("%" + search + "%")).Group("events.id").Scan(&dataEvent)
 		if tx.Error != nil {
 			return nil, tx.Error
 		}
@@ -83,12 +83,12 @@ func (repo *eventData) SelectEventComu(search string, idComu, userId int) (event
 
 	var dataEvent []event.Response
 	if search == "" {
-		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("inner join join_events on join_events.event_id = events.id").Where("events.community_id = ? ", idComu).Group("events.id").Scan(&dataEvent)
+		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("right join join_events on join_events.event_id = events.id").Where("events.community_id = ? ", idComu).Group("events.id").Scan(&dataEvent)
 		if tx.Error != nil {
 			return event.CommunityEvent{}, tx.Error
 		}
 	} else {
-		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("inner join join_events on join_events.event_id = events.id").Where("events.title like ? AND events.community_id = ? ", ("%" + search + "%"), idComu).Group("events.id").Scan(&dataEvent)
+		tx := repo.db.Model(&Community{}).Select("events.id as id, communities.logo as logo, events.title as title, count(join_events.id) as members, events.description as descriptions, events.date as date, events.price as price").Joins("inner join events on events.community_id = communities.id").Joins("right join join_events on join_events.event_id = events.id").Where("events.title like ? AND events.community_id = ? ", ("%" + search + "%"), idComu).Group("events.id").Scan(&dataEvent)
 		if tx.Error != nil {
 			return event.CommunityEvent{}, tx.Error
 		}
@@ -119,7 +119,7 @@ func (repo *eventData) SelectEventComu(search string, idComu, userId int) (event
 func (repo *eventData) SelectEventDetail(idEvent, userId int) (event.EventDetail, error) {
 
 	var data tempDetail
-	tx := repo.db.Model(&Community{}).Select("events.id as id, events.title as title, events.description as description, communities.title as penyelenggara, events.date as date, count(join_events.id) as partisipasi, events.price as price, events.location as location").Joins("inner join events on events.community_id = communities.id").Joins("inner join join_events on join_events.event_id = events.id").Where("events.id = ?", idEvent).Group("events.id").Scan(&data)
+	tx := repo.db.Model(&Community{}).Select("events.id as id, events.title as title, events.description as description, communities.title as penyelenggara, events.date as date, count(join_events.id) as partisipasi, events.price as price, events.location as location").Joins("inner join events on events.community_id = communities.id").Joins("right join join_events on join_events.event_id = events.id").Where("events.id = ?", idEvent).Group("events.id").Scan(&data)
 	if tx.Error != nil {
 		return event.EventDetail{}, tx.Error
 	}
