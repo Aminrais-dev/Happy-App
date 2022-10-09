94e3747048bfcf84aaf29251b7c41f9849d97699
“Nawi” <nawihusen@gmail.com>
2022-10-10 00:57:04 +0800

    migrate

--- migration/migrate.go
+++ migration/migrate.go
@@ -13,7 +13,7 @@ import (
 func InitMigrate(db *gorm.DB) {
 	db.AutoMigrate(cartModel.User{})
 	db.AutoMigrate(cartModel.Community{})
-	db.AutoMigrate(communityModel.JoinCommunity{})
+	db.AutoMigrate(cartModel.JoinCommunity{})
 	db.AutoMigrate(eventModel.Event{})
 	db.AutoMigrate(eventModel.JoinEvent{})
 	db.AutoMigrate(communityModel.Feed{})
